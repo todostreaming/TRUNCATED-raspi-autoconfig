5c1d1ae4fcca72879940de70a256812d5b9e1215
SHA Miao <shamiaoc@gmail.com>
2013-01-14 22:33:17 -0800

    Create raspi-autoconfig-1stboot.sh

---
+++ raspi-autoconfig-1stboot.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Part of raspi-autoconfig http://github.com/shamiao/raspi-autoconfig
+#
+# See LICENSE file for copyright and license details
+
+# Should be installed to /etc/profile.d/raspi-autoconfig-1stboot.sh to force raspi-autoconfig
+# to run at initial login
+
+# You may also want to set automatic login in /etc/inittab on tty1 by adding a
+# line such as:
+# 1:2345:respawn:/bin/login -f root tty1 </dev/tty1 >/dev/tty1 2>&1 # RPICFG_TO_DISABLE
+
+if [ $(id -u) -ne 0 ]; then
+  printf "\nNOTICE: the software on this Raspberry Pi has not been fully configured. Please run 'sudo raspi-autoconfig'. \n\n"
+else
+  raspi-autoconfig
+  exec login -f pi
+fi
